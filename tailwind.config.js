4aa917360b48d79b4830ecf7f752de8b8107b9a4
OjeeAfolabi <afolabiojee@gmail.com>
2024-03-14 15:18:47 +0100

    removed origin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "home-mobile": "url('./src/assets/home/background-home-mobile.jpg')",
+        "home-tablet": "url('./src/assets/home/background-home-tablet.jpg')",
+        "home-desktop": "url('./src/assets/home/background-home-desktop.jpg')",
+      },
+    },
   },
   plugins: [],
-}
+};
